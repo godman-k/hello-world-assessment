e737e9c36785b4500f0412d4982554cb48e949b5
Manoj Kumar Goduguluri <manoj_g@apple.com>
2024-05-31 10:27:03 -0600

    :zap: Disabling the service set to false

--- main.tf
+++ main.tf
@@ -1,35 +1,42 @@
 # Enable the necessary API services
 resource "google_project_service" "cloudfunctions" {
-  project = var.project_id
-  service = "cloudfunctions.googleapis.com"
+  project                    = var.project_id
+  service                    = "cloudfunctions.googleapis.com"
+  disable_dependent_services = false
 }
 
 resource "google_project_service" "cloudbuild" {
-  project = var.project_id
-  service = "cloudbuild.googleapis.com"
+  project                    = var.project_id
+  service                    = "cloudbuild.googleapis.com"
+  disable_dependent_services = false
 }
 
 resource "google_project_service" "artifactregistry" {
-  project = var.project_id
-  service = "artifactregistry.googleapis.com"
+  project                    = var.project_id
+  service                    = "artifactregistry.googleapis.com"
+  disable_dependent_services = false
 }
 
 resource "google_project_service" "vpcaccess" {
-  project = var.project_id
-  service = "vpcaccess.googleapis.com"
+  project                    = var.project_id
+  service                    = "vpcaccess.googleapis.com"
+  disable_dependent_services = false
 }
 
 resource "google_project_service" "sqladmin" {
-  project = var.project_id
-  service = "sqladmin.googleapis.com"
+  project                    = var.project_id
+  service                    = "sqladmin.googleapis.com"
+  disable_dependent_services = false
 }
 
 resource "google_project_service" "monitoring" {
-  project = var.project_id
-  service = "monitoring.googleapis.com"
+  project                    = var.project_id
+  service                    = "monitoring.googleapis.com"
+  disable_dependent_services = false
 }
 
 resource "google_project_service" "logging" {
-  project = var.project_id
-  service = "logging.googleapis.com"
+  project                    = var.project_id
+  service                    = "logging.googleapis.com"
+  disable_dependent_services = false
 }
